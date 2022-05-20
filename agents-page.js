444254b0582b5dba5186a68052d2de8fbee07287
MatijaBojanic <matija.bojanic@gmail.com>
2022-05-20 20:24:50 +0200

    Added agent name role and role icon

--- agents-page.js
+++ agents-page.js
@@ -12,4 +12,23 @@ fetchAgentsJSON().then( agents => {
         _img.src = this.src;
     }
     newImg.src = imageSrc;
+    agentDescription(agents[4])
 });
+
+function agentDescription(agent){
+    var agentName = document.getElementById('agentName');
+    var text = document.createTextNode(agent.displayName);
+    agentName.appendChild(text);
+    var agentRole = document.getElementById('agentRole');
+    var text = document.createTextNode(agent.role.displayName);
+    agentName.appendChild(text);
+
+    let imageSrc = agent.role.displayIcon
+    let _img = document.getElementById('roleIcon');
+    let newImage = new Image;
+    newImage.onload = function() {
+        _img.src = this.src;
+    }
+    newImage.src = imageSrc;
+
+}
