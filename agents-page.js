5b56cd56fd7afc4afa94d7955c693c981572a258
MatijaBojanic <matija.bojanic@gmail.com>
2022-05-20 20:21:33 +0200

    Added agent picture. We load agent and background

--- agents-page.js
+++ agents-page.js
@@ -5,13 +5,11 @@ async function fetchAgentsJSON(){
 }
 
 fetchAgentsJSON().then( agents => {
-    console.log(agents[0]);
-    let imageSrc = agents[0].bustPortrait
+    let imageSrc = agents[4].fullPortraitV2
     var _img = document.getElementById('id1');
     var newImg = new Image;
     newImg.onload = function() {
         _img.src = this.src;
     }
     newImg.src = imageSrc;
-
 });
