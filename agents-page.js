0421e213dac96dddadcffeb0504922cd441af8e6
MatijaBojanic <matija.bojanic@gmail.com>
2022-05-20 20:27:49 +0200

    Ability Video description

--- agents-page.js
+++ agents-page.js
@@ -13,12 +13,15 @@ fetchAgentsJSON().then( agents => {
     }
     newImg.src = imageSrc;
     agentDescription(agents[4])
+    loadAbilityIcons(agents[4])
+    abilityDescription(agents[4])
 });
 
 function agentDescription(agent){
     var agentName = document.getElementById('agentName');
     var text = document.createTextNode(agent.displayName);
     agentName.appendChild(text);
+    // roleIcon, agentRole
     var agentRole = document.getElementById('agentRole');
     var text = document.createTextNode(agent.role.displayName);
     agentName.appendChild(text);
@@ -31,4 +34,26 @@ function agentDescription(agent){
     }
     newImage.src = imageSrc;
 
+}
+
+function loadAbilityIcons(agent){
+    for(i = 0; i < 4; i++){
+        let imageSrc = agent.abilities[i].displayIcon
+        let _img = document.getElementById('ability'+(i+1));
+        let newImage = new Image;
+        newImage.onload = function() {
+            _img.src = this.src;
+        }
+        newImage.src = imageSrc;
+    }
+}
+
+function abilityDescription(agent){
+    var abilityDescriptionElement = document.getElementById("abilityDescription");
+    var text = document.createTextNode(agent.abilities[0].description);
+    abilityDescriptionElement.appendChild(text);
+
+    var abilityNameElement = document.getElementById("abilityName");
+    var titleName = document.createTextNode(agent.abilities[0].displayName);
+    abilityNameElement.appendChild(titleName);
 }
